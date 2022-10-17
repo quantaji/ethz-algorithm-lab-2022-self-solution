b874b1c63bb29daf06f4504a4bbb9c641e238a31
Guangda Ji <quanta.guangda.ji@gmail.com>
2022-10-17 21:57:14 +0200

    mod

--- week 05/00_tracking/src/main.cpp
+++ week 05/00_tracking/src/main.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <algorithm>
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, boost::property<boost::edge_weight_t, int>> Graph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::no_property, boost::property<boost::edge_weight_t, int>> Graph;
 typedef boost::property_map<Graph, boost::edge_weight_t>::type weight_map; // this is one way for definint weight
 typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
 typedef boost::graph_traits<Graph>::edge_descriptor            edge_desc;
@@ -15,7 +16,9 @@ typedef boost::graph_traits<Graph>::out_edge_iterator         out_edge_it;
 
 void solve() {
   int n, m, k, x, y; std::cin >> n >> m >> k >> x >> y;
-  int K = 15; // reducing this can reduce the amount of time
+  int K = 15; // reducing this can reduce the amount of time // but, theoretically, this is not guaranteed to be true. 
+  // for undirected graph, with neiboring layer connected, the k considered ranges from k to 2K - k (consider reflection)
+  // K = 15 gives a very descently fast solution...
   Graph G(n*(K+1));
   for (int i=0; i<m; ++i){
     int a, b, c, d; std::cin >> a >> b >> c >> d;
@@ -23,36 +26,21 @@ void solve() {
       if (i<K){
         boost::add_edge((K+1)*a+i, (K+1)*b+i+1, EdgeWeightProperty(c), G);
         boost::add_edge((K+1)*b+i, (K+1)*a+i+1, EdgeWeightProperty(c), G);
-        // and additional edges to jump back
-        boost::add_edge((K+1)*b+i+1, (K+1)*a+i,  EdgeWeightProperty(c), G);
-        boost::add_edge((K+1)*a+i+1, (K+1)*b+i, EdgeWeightProperty(c), G);
       }
-      // and additional edges to jump back
+      // // and additional edges to jump back
       boost::add_edge((K+1)*a+i, (K+1)*b+i, EdgeWeightProperty(c), G);
-      boost::add_edge((K+1)*b+i, (K+1)*a+i, EdgeWeightProperty(c), G);
     }
     else for (int i=0; i<=K; ++i){
       boost::add_edge((K+1)*a+i, (K+1)*b+i, EdgeWeightProperty(c), G);
-      boost::add_edge((K+1)*b+i, (K+1)*a+i, EdgeWeightProperty(c), G);
     }
-    // this might be right or wrong
-    // if (d==1) {
-    //   for (int i=0; i<K; ++ i) for (int j=0; j<K; ++k){
-    //     if (i > j){
-    //       boost::add_edge(K*a + i, K*b+j,EdgeWeightProperty(c), G);
-    //       if (i == j + 1) boost::add_edge(K*b+j, K*a + i, EdgeWeightProperty(c), G);
-    //     }
-    //     if (i < j){
-    //       boost::add_edge(K*b+j, K*a + i,EdgeWeightProperty(c), G);
-    //       if (j == i + 1) boost::add_edge(K*a+i, K*b+j,EdgeWeightProperty(c), G);
-    //     }
-    //   }
-    // } else {
-    //   for (int i=0; i<K; ++ i) boost::add_edge(K*a+i, K*b+i,EdgeWeightProperty(c), G);
-    // }
   }
   std::vector<int> dist_map(n*(K+1));
   boost::dijkstra_shortest_paths(G, x*(K+1), boost::distance_map(boost::make_iterator_property_map(dist_map.begin(), boost::get(boost::vertex_index, G))));
+  
+  // int min_cost = dist_map[y*(K+1)+k];
+  // for (int i=k+1; i<=K; ++i) min_cost = std::min(min_cost, dist_map[y*(K+1)+i]);
+  
+  // std::cout << min_cost << std::endl;
   std::cout << dist_map[y*(K+1)+k] << std::endl;
   
 }
